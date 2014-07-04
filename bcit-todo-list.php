032f2f1618a619e346ecd041b9f01389101ecef7
Curtis McHale <curtis@curtismchale.ca>
2014-07-03 20:48:14 -0700

    - check caps and make sure the shortcode works
    Now we're checking to make sure that a user can either edit or add a
    todo item. If they can they should be able to see our form.
    
    For now we're just returning 'something' to make sure that our shortcode
    is in fact functioning.
    
    Remember that shortcodes need to return stuff always or it's going to
    end up above you're content and you'll look silly.

--- bcit-todo-list.php
+++ bcit-todo-list.php
@@ -25,6 +25,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+require_once( 'inc/shortcodes.php' );
+
 class BCIT_TODO_List{
 
 	function __construct(){
--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -4,7 +4,21 @@ class BCIT_TODO_shortcodes{
 
 	function __construct(){
 
+		add_shortcode( 'bcit_todo_add_item_form', array( $this, 'add_item_form' ) );
+
 	} // __construct
+
+	public function add_item_form(){
+
+		if ( current_user_can( 'create_todo_list' ) || current_user_can( 'edit_todo_list' ) ) {
+
+			$html = 'something';
+
+		} // if current_user_can
+
+		return $html;
+
+	} // add_item_form
 
 } // BCIT_TODO_shortcodes
 

4881913f5fee3af0e00410ccd604c9d14bb412de
Curtis McHale <curtis@curtismchale.ca>
2014-07-03 22:44:31 -0700

    - now we're actually making an AJAX request
    first we push all our JS vars down to a single one to pass along to our
    AJAX request.
    
    Then we're actually making the request. Of course I've got something
    that works (as in it does nothing properly) first then we'll dig in to
    actually processing the form.

--- assets/frontend-scripts.js
+++ assets/frontend-scripts.js
@@ -9,6 +9,18 @@ jQuery(document).ready(function($) {
 		var title       = $( form ).find( '#bcit-todo-item' ).val();
 		var description = $( form ).find( '#bcit-todo-item-description' ).val();
 
+		var data = {
+			action: action,
+			title: title,
+			description: description
+		}
+
+		$.post( BCITTODO.ajaxurl, data, function( response ) {
+
+			console.log(response);
+
+		}); // end AJAX post
+
 
 	});
 

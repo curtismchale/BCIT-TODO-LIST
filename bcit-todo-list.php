6928e0e408a7332adbee8611421f5b1c32c4fa6d
Curtis McHale <curtis@curtismchale.ca>
2014-07-03 22:51:54 -0700

    - catching our JS with a proper AJAX server side thingy
    Now we are catching our AJAX request properly. Notice that WordPress
    needs 2 hooks.
    
    1. wp_ajax_*
    2. wp_ajax_nopriv_*
    
    The special one is the 2nd one. It runs when a user isn't signed in to a
    site. So you really only need it if you need this AJAX action to be
    processed by users that aren't logged in to your site.
    
    Simply excluding the nopriv version isn't enough for security though. We
    still need to nonce this and check our caps and escape everything. Which
    we'll do.
    
    Really this is a minimum function to make sure we wrote decent code. We
    used wp_send_json_success so that our console.log statment will return
    the success object which is different than the 0 (or failure) we saw
    in our last commit.

--- bcit-todo-list.php
+++ bcit-todo-list.php
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 require_once( 'inc/shortcodes.php' );
+require_once( 'inc/ajax-requests.php' );
 
 class BCIT_TODO_List{
 
---
+++ inc/ajax-requests.php
@@ -0,0 +1,24 @@
+<?php
+
+class BCIT_TODO_Ajax_Requests{
+
+	function __construct(){
+		add_action( 'wp_ajax_bcit_todo_add_todo_item', array( $this, 'process_todo_item' ) );
+		add_action( 'wp_ajax_nopriv_bcit_todo_add_todo_item', array( $this, 'process_todo_item' ) );
+	} // __construct
+
+	/**
+	 * Processes the submitted TODO item
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 */
+	public function process_todo_item(){
+
+		wp_send_json_success();
+
+	} // process_todo_item
+
+} // BCIT_TODO_Ajax_Requests
+
+new BCIT_TODO_Ajax_Requests();
